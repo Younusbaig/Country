f3489f14989798231c8018ad53b5a62561dae25e
Muhammad Younus Baig <younus@Muhammads-MacBook-Pro.local>
2024-07-16 20:19:55 +0100

    test cases fail

--- src/main/java/com/Country/service/impl/CountryServiceImpl.java
+++ src/main/java/com/Country/service/impl/CountryServiceImpl.java
@@ -1,39 +1,43 @@
 package com.country.service.impl;
-
-import com.country.config.RestClient;
 import com.country.dto.CountryDto;
 import com.country.exception.ServiceException;
 import com.country.service.CountryService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class CountryServiceImpl implements CountryService {
 
-   private final RestClient restClient;
-   private final String countriesApiUrL;
+    private final RestClient restClient;
+    private final String countriesApiUrL;
 
     public CountryServiceImpl(RestClient restClient, @Value("${rest.countries.api.url}") String countriesApiUrL) {
         this.restClient = restClient;
         this.countriesApiUrL = countriesApiUrL;
     }
 
-
     @Override
     public CountryDto getByCountryName(String name) {
+
         String url = UriComponentsBuilder.fromHttpUrl(countriesApiUrL)
                 .buildAndExpand(name)
                 .toUriString();
-        CountryDto[] countryDto = restClient.getForObject(url, CountryDto[].class);
+        CountryDto[] countryDto = restClient.get()
+                .uri(url)
+                .retrieve()
+                .body(CountryDto[].class);
 
-        if (countryDto != null && countryDto.length > 0) {
+
+        if (countryDto != null) {
             return countryDto[0];
         } else {
             throw new ServiceException("Country name not found");
         }
     }
-    }
+}
+
 
 
 
--- src/test/java/com/country/service/impl/CountryServiceImplTest.java
+++ src/test/java/com/country/service/impl/CountryServiceImplTest.java
@@ -1,17 +1,17 @@
 package com.country.service.impl;
 
-import com.country.config.RestClient;
 import com.country.dto.CountryDto;
-import com.country.exception.ServiceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
+
 
 public class CountryServiceImplTest {
 
@@ -27,38 +27,24 @@ public class CountryServiceImplTest {
     void setUp(){
         MockitoAnnotations.openMocks(this);
         countryService = new CountryServiceImpl(restClient, restCountriesApiUrl);
+
     }
+
+
     @Test
     public void testGetByCountryName_Success() {
-    String countryName = "Germany";
-    String url = UriComponentsBuilder.fromHttpUrl(restCountriesApiUrl)
-            .buildAndExpand(countryName)
-            .toUriString();
-
-    CountryDto[] response = { new CountryDto() }; // Mock response
-    when(restClient.getForObject(url, CountryDto[].class)).thenReturn(response);
-
-    CountryDto result = countryService.getByCountryName(countryName);
-
-    assertNotNull(result);
-    verify(restClient, times(1)).getForObject(url, CountryDto[].class);
-    }
-
-
-    @Test
-    public void testGetByCountryName_NotFound() {
-        String countryName = "NonExistentCountry";
+        String countryName = "Germany";
         String url = UriComponentsBuilder.fromHttpUrl(restCountriesApiUrl)
                 .buildAndExpand(countryName)
                 .toUriString();
 
-        when(restClient.getForObject(url, CountryDto[].class)).thenReturn(new CountryDto[0]);
+        CountryDto[] response = {new CountryDto()}; // Mock response
+        when(restClient.get().uri(url).retrieve().body(CountryDto[].class)).thenReturn(response);
 
-        ServiceException exception = assertThrows(ServiceException.class, () -> {
-            countryService.getByCountryName(countryName);
-        });
+        CountryDto result = countryService.getByCountryName(countryName);
 
-        assertEquals("Country name not found", exception.getMessage());
-        verify(restClient, times(1)).getForObject(url, CountryDto[].class);
+        assertNotNull(result);
+        verify(restClient, times(1)).get().uri(url, CountryDto[].class);
+
     }
 }
